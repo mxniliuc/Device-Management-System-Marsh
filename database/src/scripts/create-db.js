4fbdfd908421e0224d3199e83cd6cb9e7cbc2614
Laur Maniliuc <laurentiumaniliuc1@Laurs-MacBook-Air.local>
2026-04-01 12:26:17 +0300

    Added the first database script used for its creation.

---
+++ database/src/scripts/create-db.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+
+function getMongoUri() {
+  return process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/device_management';
+}
+
+function getDbNameFromUri(uri) {
+  const url = new URL(uri);
+  const pathname = url.pathname || '';
+  const dbFromPath = pathname.replace(/^\//, '');
+  if (!dbFromPath) {
+    throw new Error('MONGODB_URI must include a database name (e.g. mongodb://127.0.0.1:27017/device_management)');
+  }
+  return dbFromPath;
+}
+
+async function run() {
+  const uri = getMongoUri();
+  const dbName = getDbNameFromUri(uri);
+
+  const client = new MongoClient(uri);
+  await client.connect();
+
+  try {
+    const db = client.db(dbName);
+
+    const users = db.collection('users');
+    const devices = db.collection('devices');
+
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  process.exitCode = 1;
+});
+
